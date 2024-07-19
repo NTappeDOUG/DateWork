7dc1cf1cc04c2228821303c0c08c6eefffd6a395
Luka Wölk <l.woelk@douglas.de>
2024-07-19 09:31:26 +0200

    push Base

---
+++ backup.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
